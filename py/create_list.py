78f907d58aab4ca21881c5db2451e57b1399ad04
flopp <fpigorsch@gmail.com>
2018-04-05 19:00:50 +0200

    use image placeholder if there's no image

--- py/create_list.py
+++ py/create_list.py
@@ -2,6 +2,14 @@ import math
 import os
 from utilities import chunks, load_template
 
+NO_IMAGE_DATA = \
+    "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A" \
+    "%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%" \
+    "22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16296b729b2%20text%20%7B%20fill%3A%23ec" \
+    "eeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20" \
+    "monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16296b729b2%22%" \
+    "3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3C" \
+    "text%20x%3D%22100%22%20y%3D%22250%22%3EKein%20Bild%20%3A(%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
 
 def create_file_name(index, count):
     if index < 0 or index >= count:
@@ -57,7 +65,7 @@ def create_cache_item(template, cache):
     desc = ''
     if cache._short_description is not None:
         desc = cache._short_description
-    img = ''
+    img = NO_IMAGE_DATA
     thumb = '.cache/big/{}.jpg'.format(cache._code)
     if os.path.exists(thumb):
         img = 'img/big/{}.jpg'.format(cache._code)
