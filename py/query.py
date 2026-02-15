e263d4f94febbc0b5262d3b497200cd0ea5a370a
Florian Pigorsch <fpigorsch@gmail.com>
2026-02-15 15:01:07 +0100

    use post

--- py/query.py
+++ py/query.py
@@ -5,6 +5,8 @@ import re
 def download_query(user, password, queryid):
     headers = {'User-agent': 'safari-map [https://safari.flopp.net/]'}
     s = requests.Session()
+
+    print("Logging in as {}...".format(user))
     r = s.post(
         'https://www.opencaching.de/login.php',
         data={
@@ -24,15 +26,17 @@ def download_query(user, password, queryid):
     batch_size = 20
     batch_start = 0
     while True:
+        print("Downloading batch {} ({}-{})".format(batch_start // batch_size, batch_start, batch_start + batch_size))
         url = 'https://www.opencaching.de/search.php?queryid={}&output=loc&startat={}&count={}&zip=0'
         url = url.format(queryid, batch_start, batch_size)
-        r = s.get(url, headers=headers)
+        r = s.post(url, headers=headers)
         if r.status_code != 200:
             print("-- Terminating due to bad status code: {}".format(r.status_code))
             break
         new_oc_codes = []
         for m in re.finditer(r'<name id="([^"]+)">', r.text):
             new_oc_codes.append(m.groups()[0])
+        print("  -> found {} new OC codes".format(len(new_oc_codes)))
         if len(new_oc_codes) == 0:
             break
         oc_codes = oc_codes + new_oc_codes
